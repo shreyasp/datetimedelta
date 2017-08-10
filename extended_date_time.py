0615e7e59c032dc88026e5e31ad7798d37ed4027
Shreyas Patil <shreyasp@users.noreply.github.com>
2017-08-11 02:35:51 +0530

    Initial commit

---
+++ extended_date_time.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+# This module gives extended functionality for calculating datetime based on
+# functionality that exists in Ruby
+from datetime import datetime
+import sys
+
+class ExtendedDateTime(int):
+
+    def __new__(cls, *args, **kwargs):
+        print(cls, *args)
+        _instance = int.__new__(cls, *args, **kwargs)
+        setattr(_instance, 'current_date', datetime.now())
+        return _instance
+
+    def __getattr__(self, attr):
+        if attr == 'ago':
+            print('ATTR: ', attr)
+
+        else:
+            return
+
+    @property
+    def day(self):
+        return self
+
+    @property
+    def week(self):
+        return self
+
+    @property
+    def month(self):
+        return self
+
+
+def main():
+    ex = ExtendedDateTime(-12)
+    print('Value: ', ex)
+
+    print('Day: ', ex.day)
+    print('Week: ', ex.week)
+    print('Month: ', ex.month)
+
+    try:
+        print('Chained: ', ex.day.week.month.ago)
+
+    except Exception as exp:
+        print(exp)
+        sys.exit(-1)
+
+
+if __name__ == '__main__':
+    main()
