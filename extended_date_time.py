7282e3c46e2dea307d9909ecf38560ffb4aa6a2d
Shreyas Patil <shreyasp@users.noreply.github.com>
2017-08-13 11:41:53 +0530

    [Minor] Merge from calc-delat
    * [Minor] Method Chaining along with setting attributes for calculating correct timedelta
    
    * [Minor] Relative Date calculation based on current date
    
    * [House-keeping] Code Cleanup
    
    * [Minor] Cleanup

--- extended_date_time.py
+++ extended_date_time.py
@@ -2,49 +2,83 @@
 # This module gives extended functionality for calculating datetime based on
 # functionality that exists in Ruby
 from datetime import datetime
+from dateutil.relativedelta import relativedelta
 import sys
 
+
 class ExtendedDateTime(int):
+    STANDARD_ATTRS = ['_calc_day', '_calc_week', '_calc_month']
+
     def __new__(cls, *args, **kwargs):
         _instance = int.__new__(cls, *args, **kwargs)
-        setattr(_instance, 'current_date', datetime.now())
+        for attr in cls.STANDARD_ATTRS:
+            setattr(_instance, attr, False)
+
         return _instance
 
     def __getattr__(self, attr):
         if attr == 'ago':
-            print('ATTR: ', attr)
+            _calc_factor = -1
 
-        else:
-            return
+        elif attr == 'after':
+            _calc_factor = 1
+
+        self._calc_date_x(_calc_factor)
+        return self._resultant_data
 
     @property
     def day(self):
+        setattr(self, '_calc_day', True)
+        setattr(self, '_calc_week', False)
+        setattr(self, '_calc_month', False)
         return self
 
     @property
     def week(self):
+        setattr(self, '_calc_week', True)
+        setattr(self, '_calc_day', False)
+        setattr(self, '_calc_month', False)
         return self
 
     @property
     def month(self):
+        setattr(self, '_calc_day', False)
+        setattr(self, '_calc_week', False)
+        setattr(self, '_calc_month', True)
         return self
 
+    def _calc_date_x(self, _calc_factor):
+        if(getattr(self, '_calc_day')):
+            _relative_delta = relativedelta(days=_calc_factor * self)
 
-def main():
-    ex = ExtendedDateTime(12)
-    print('Value: ', ex)
+        elif(getattr(self, '_calc_week')):
+            _relative_delta = relativedelta(weeks=_calc_factor * self)
 
-    print('Day: ', ex.day)
-    print('Week: ', ex.week)
-    print('Month: ', ex.month)
+        elif(getattr(self, '_calc_month')):
+            _relative_delta = relativedelta(months=_calc_factor * self)
+
+        _resultant_data = datetime.now() + _relative_delta
+        setattr(self, '_resultant_data', _resultant_data)
+
+
+def test_func():
+    ex = ExtendedDateTime(1)
+    print('Gap: ', ex)
 
     try:
-        print('Chained: ', ex.day.week.month.ago)
+        print('Day Ago: ', ex.day.ago)
+        print('Day After: ', ex.day.after)
+
+        print('Week Ago: ', ex.week.ago)
+        print('Week After: ', ex.week.after)
+
+        print('Month Ago: ', ex.month.ago)
+        print('Month After: ', ex.month.after)
 
     except Exception as exp:
-        print(exp)
+        print('Error: ', exp)
         sys.exit(-1)
 
 
 if __name__ == '__main__':
-    main()
+    test_func()
