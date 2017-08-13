9fb18cc6608250bec147a808a9f218097ddead87
Shreyas Patil <shreyasp@users.noreply.github.com>
2017-08-13 13:42:56 +0530

    [Minor] Added Years as computational property

--- datetimedelta/extended_date_time.py
+++ datetimedelta/extended_date_time.py
@@ -3,15 +3,14 @@
 # functionality that exists in Ruby
 from datetime import datetime
 from dateutil.relativedelta import relativedelta
-import sys
 
 
 class ExtendedDateTime(int):
-    STANDARD_ATTRS = ['_calc_day', '_calc_week', '_calc_month']
+    _DELTA_ATTRS = ['_calc_days', '_calc_weeks', '_calc_months', '_calc_years']
 
     def __new__(cls, *args, **kwargs):
         _instance = int.__new__(cls, *args, **kwargs)
-        for attr in cls.STANDARD_ATTRS:
+        for attr in cls._DELTA_ATTRS:
             setattr(_instance, attr, False)
 
         return _instance
@@ -25,40 +24,48 @@ class ExtendedDateTime(int):
 
         self._calc_date_x(_calc_factor)
         self._reset_attributes()
-        return self._resultant_data
+        return self._resultant_date
 
     def _reset_attributes(self):
-        for attr in self.STANDARD_ATTRS:
+        for attr in self._DELTA_ATTRS:
             setattr(self, attr, False)
         return self
 
     @property
     def days(self):
-        setattr(self, '_calc_day', True)
+        setattr(self, '_calc_days', True)
         return self
 
     @property
     def weeks(self):
-        setattr(self, '_calc_week', True)
+        setattr(self, '_calc_weeks', True)
         return self
 
     @property
     def months(self):
-        setattr(self, '_calc_month', True)
+        setattr(self, '_calc_months', True)
+        return self
+
+    @property
+    def years(self):
+        setattr(self, '_calc_years', True)
         return self
 
     def _calc_date_x(self, _calc_factor):
-        if(getattr(self, '_calc_day')):
+        if(getattr(self, '_calc_days')):
             _relative_delta = relativedelta(days=_calc_factor * self)
 
-        elif(getattr(self, '_calc_week')):
+        elif(getattr(self, '_calc_weeks')):
             _relative_delta = relativedelta(weeks=_calc_factor * self)
 
-        elif(getattr(self, '_calc_month')):
+        elif(getattr(self, '_calc_months')):
             _relative_delta = relativedelta(months=_calc_factor * self)
 
-        else:
-            raise Exception('Need days or weeks or months to compute datetime delta')
+        elif(getattr(self, '_calc_years')):
+            _relative_delta = relativedelta(years=_calc_factor * self)
 
-        _resultant_data = datetime.now() + _relative_delta
-        setattr(self, '_resultant_data', _resultant_data)
+        else:
+            raise Exception('Need days or weeks or months or years to compute datetime delta')
+
+        _resultant_date = datetime.now() + _relative_delta
+        setattr(self, '_resultant_date', _resultant_date)
--- datetimedelta/tests/test_extended_date_time.py
+++ datetimedelta/tests/test_extended_date_time.py
@@ -3,91 +3,111 @@ from datetimedelta.extended_date_time import ExtendedDateTime
 from datetime import datetime
 from dateutil.relativedelta import relativedelta
 
+
 class TestExtendedDateTime(TestCase):
 
-	# Check whether instance is of type Date time
-	def test_instance_type(self):
-		extend_date_time_obj = ExtendedDateTime(2)
-		self.assertIsInstance(extend_date_time_obj.days.ago, datetime)
+    # Check whether instance is of type Date time
+    def test_instance_type(self):
+        extend_date_time_obj = ExtendedDateTime(2)
+        self.assertIsInstance(extend_date_time_obj.days.ago, datetime)
 
-	# Check whether Days.ago & Days.After are evaluated correctly
-	def test_days_delta(self):
-		days_gap = ExtendedDateTime(2)
+    # Check whether Days.ago & Days.After are evaluated correctly
+    def test_days_ago_delta(self):
+        days_gap = ExtendedDateTime(2)
 
-		current_date = datetime.now().date()
-		delta = relativedelta(days=-2)
-		expected_date = current_date + delta
-		calculated_date = days_gap.days.ago.date()
+        current_date = datetime.now().date()
+        delta = relativedelta(days=-2)
+        expected_date = current_date + delta
+        calculated_date = days_gap.days.ago.date()
 
-		self.assertEqual(calculated_date, expected_date)
+        self.assertEqual(calculated_date, expected_date)
 
-	def test_days_after_delta(self):
-		days_gap = ExtendedDateTime(2)
+    def test_days_after_delta(self):
+        days_gap = ExtendedDateTime(2)
 
-		current_date = datetime.now().date()
-		delta = relativedelta(days=2)
-		expected_date = current_date + delta
-		calculated_date = days_gap.days.after.date()
+        current_date = datetime.now().date()
+        delta = relativedelta(days=2)
+        expected_date = current_date + delta
+        calculated_date = days_gap.days.after.date()
 
-		self.assertEqual(calculated_date, expected_date)
+        self.assertEqual(calculated_date, expected_date)
 
-	# Check whether Weeks.ago & Weeks.after are evaluated correctly
-	def test_weeks_ago_delta(self):
-		weeks_gap = ExtendedDateTime(3)
+    # Check whether Weeks.ago & Weeks.after are evaluated correctly
+    def test_weeks_ago_delta(self):
+        weeks_gap = ExtendedDateTime(3)
 
-		current_date = datetime.now().date()
-		delta = relativedelta(weeks=-3)
-		expected_date = current_date + delta
-		calculated_date = weeks_gap.weeks.ago.date()		
+        current_date = datetime.now().date()
+        delta = relativedelta(weeks=-3)
+        expected_date = current_date + delta
+        calculated_date = weeks_gap.weeks.ago.date()
 
-		self.assertEqual(calculated_date, expected_date)
+        self.assertEqual(calculated_date, expected_date)
 
-	def test_weeks_after_delta(self):
-		weeks_gap = ExtendedDateTime(3)
-		
-		current_date = datetime.now().date()
-		delta = relativedelta(weeks=3)
-		expected_date = current_date + delta
-		calculated_date = weeks_gap.weeks.after.date()		
+    def test_weeks_after_delta(self):
+        weeks_gap = ExtendedDateTime(3)
 
-		self.assertEqual(calculated_date, expected_date)
+        current_date = datetime.now().date()
+        delta = relativedelta(weeks=3)
+        expected_date = current_date + delta
+        calculated_date = weeks_gap.weeks.after.date()
 
-	# Check whether Months.ago & Months.after are evaluated correctly
-	def test_months_ago_delta(self):
-		months_gap = ExtendedDateTime(4)
+        self.assertEqual(calculated_date, expected_date)
 
-		current_date = datetime.now().date()
-		delta = relativedelta(months=-4)
-		expected_date = current_date + delta
-		calculated_date = months_gap.months.ago.date()		
+    # Check whether Months.ago & Months.after are evaluated correctly
+    def test_months_ago_delta(self):
+        months_gap = ExtendedDateTime(4)
 
-		self.assertEqual(calculated_date, expected_date)
+        current_date = datetime.now().date()
+        delta = relativedelta(months=-4)
+        expected_date = current_date + delta
+        calculated_date = months_gap.months.ago.date()
 
-	def test_months_after_delta(self):
-		months_gap = ExtendedDateTime(4)
-		
-		current_date = datetime.now().date()
-		delta = relativedelta(months=4)
-		expected_date = current_date + delta
-		calculated_date = months_gap.months.after.date()		
+        self.assertEqual(calculated_date, expected_date)
 
-		self.assertEqual(calculated_date, expected_date)
+    def test_months_after_delta(self):
+        months_gap = ExtendedDateTime(4)
 
+        current_date = datetime.now().date()
+        delta = relativedelta(months=4)
+        expected_date = current_date + delta
+        calculated_date = months_gap.months.after.date()
 
-	# Check whether proper exceptions raised for no days, weeks, months given
-	def test_no_delta_type_ago_exception(self):
-		extend_date_time_obj = ExtendedDateTime(2)	
-		with self.assertRaises(Exception) as context:
-			extend_date_time_obj.ago
+        self.assertEqual(calculated_date, expected_date)
 
-		self.assertTrue('Need days or weeks or months to compute datetime delta',
-			str(context.exception))
+    # Check whether Years.ago & Years.after are evaluated correctly
+    def test_years_ago_delta(self):
+        years_gap = ExtendedDateTime(1)
 
+        current_date = datetime.now().date()
+        delta = relativedelta(years=-1)
+        expected_date = current_date + delta
+        calculated_date = years_gap.years.ago.date()
 
-	def test_no_delta_type_after_exception(self):
-		extend_date_time_obj = ExtendedDateTime(2)
-		with self.assertRaises(Exception) as context:
-			extend_date_time_obj.after
+        self.assertEqual(calculated_date, expected_date)
 
-		self.assertTrue('Need days or weeks or months to compute datetime delta',
-			str(context.exception))
+    def test_years_after_delta(self):
+        years_gap = ExtendedDateTime(1)
+
+        current_date = datetime.now().date()
+        delta = relativedelta(years=1)
+        expected_date = current_date + delta
+        calculated_date = years_gap.years.after.date()
+
+        self.assertEqual(calculated_date, expected_date)
+
+    # Check whether proper exceptions raised for no days, weeks, months given
+    def test_no_delta_type_ago_exception(self):
+        extend_date_time_obj = ExtendedDateTime(2)
+        with self.assertRaises(Exception) as context:
+            extend_date_time_obj.ago
+
+        self.assertTrue('Need days or weeks or months or years to compute datetime delta',
+            str(context.exception))
+
+    def test_no_delta_type_after_exception(self):
+        extend_date_time_obj = ExtendedDateTime(2)
+        with self.assertRaises(Exception) as context:
+            extend_date_time_obj.after
+
+        self.assertTrue('Need days or weeks or months or years to compute datetime delta',
+            str(context.exception))
