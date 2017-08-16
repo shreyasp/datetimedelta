99075c05b685e305952ed1283568f1efa3d6bccb
Shreyas Patil <shreyasp@users.noreply.github.com>
2017-08-16 11:00:45 +0530

    [Minor] Restricting Python version to 3.5 for Package installation

--- setup.py
+++ setup.py
@@ -1,26 +1,27 @@
 from setuptools import setup
 
 setup(
-	name='datetimedelta',
-	version='0.0.1',
-	description='Extends Ruby date time delta functionality to Python',
-	author='Shreyas Patil',
-	author_email='shreyaspatil87@gmail.com',
-	packages=[
-		'datetimedelta'
-	],
-	url='https://github.com/shreyasp/datetimedelta',
-	license='MIT',
-	install_requires=[
-		'python-dateutil'
-	],
-	keywords=[
-		'datetime',
-		'delta',
-		'inline'
-	],
-	test_suite='nose.collector',
-	tests_require=[
-		'nose'
-	]
+    name='datetimedelta',
+    version='0.0.1',
+    description='Extends Ruby date time delta functionality to Python',
+    author='Shreyas Patil',
+    author_email='shreyaspatil87@gmail.com',
+    packages=[
+        'datetimedelta'
+    ],
+    url='https://github.com/shreyasp/datetimedelta',
+    license='MIT',
+    install_requires=[
+        'python-dateutil'
+    ],
+    keywords=[
+        'datetime',
+        'delta',
+        'inline'
+    ],
+    test_suite='nose.collector',
+    tests_require=[
+        'nose'
+    ],
+    python_requires='>3.5'
 )
